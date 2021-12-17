80e607ba7e0c06a5a6b518ebd5a7bd74ae4b823a
Idoko Agbo <idokojudge@gmail.com>
2021-12-16 19:33:33 -0500

    counting power of strings

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -3,4 +3,26 @@
 
 # String Formatting
 
+
+# counting the power of strings 
+
+string = 'cc'
+
+count=1
+temp_count = 1
+
+for x in range(0, len(string)-1):
+
+    if(string[x] == string[x+1]):
+        temp_count+=1
+
+    else:
+        temp_count=1
+
+    if(count < temp_count):
+        count = temp_count
+
+
+print(f'String power: {count}')
+
 # String Methods
